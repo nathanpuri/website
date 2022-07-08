9ed096365cce25054757eef9f1ed9edfb691be46
Nathan Puri <>
2022-07-08 23:41:25 +0100

    WIP localization for Brazil

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,23 @@
 import { defineConfig } from "astro/config";
+import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [],
+  experimental: {
+    integrations: true,
+  },
+  integrations: [
+    astroI18next({
+      baseLanguage: "en",
+      i18next: {
+        debug: true, // convenient during development to check for missing keys
+        supportedLngs: ["en", "pt"],
+        initImmediate: false,
+        backend: {
+          loadPath: "./src/locales/{{lng}}.json",
+        },
+      },
+      i18nextPlugins: { fsBackend: "i18next-fs-backend" },
+    }),
+  ],
 });
