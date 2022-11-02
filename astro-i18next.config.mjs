2b63769f993e7857135d4b6932cfe94b7577e3ce
Nathan Puri <>
2022-11-02 18:08:01 +0000

    update to Astro v1.6.2, update astro-i18next, add hint to slug in config

---
+++ astro-i18next.config.mjs
@@ -0,0 +1,12 @@
+export default {
+  defaultLanguage: 'en',
+  supportedLanguages: ['en', 'pt'],
+  i18next: {
+    debug: true, // convenient during development to check for missing keys
+    initImmediate: false,
+    backend: {
+      loadPath: './src/locales/{{lng}}.json',
+    },
+    i18nextPlugins: { fsBackend: 'i18next-fs-backend' },
+  },
+}
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,7 @@
-import { defineConfig } from "astro/config";
-import astroI18next from "astro-i18next";
+import { defineConfig } from 'astro/config'
+import astroI18next from 'astro-i18next'
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    integrations: true,
-  },
-  integrations: [
-    astroI18next({
-      baseLanguage: "en",
-      i18next: {
-        debug: true, // convenient during development to check for missing keys
-        supportedLngs: ["en", "pt"],
-        initImmediate: false,
-        backend: {
-          loadPath: "./src/locales/{{lng}}.json",
-        },
-      },
-      i18nextPlugins: { fsBackend: "i18next-fs-backend" },
-    }),
-  ],
-});
+  integrations: [astroI18next()],
+})
