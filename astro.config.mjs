caf4e3989c9b34b709d67332ee621a8844b1441f
Nathan Puri <pnpuri@protonmail.com>
2023-10-17 12:19:48 +0100

    replace 'base' with 'site'

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config'
 import astroI18next from 'astro-i18next'
 
-const baseUrl = process?.env?.PUBLIC_BASE_URL || '/'
-console.log('baseUrl', baseUrl)
+const site = process?.env?.PUBLIC_BASE_URL || '/'
+console.log('site', site)
 console.log('process.env', process?.env)
 
 export default defineConfig({
   integrations: [astroI18next()],
-  base: baseUrl,
+  site,
 })
