a5996ba559e22c4cb0a293c6250884868c8852b3
Nathan Puri <pnpuri@protonmail.com>
2023-10-17 12:30:35 +0100

    remove logging, add redirect for /en/cerca-de

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from 'astro/config'
 import astroI18next from 'astro-i18next'
 
 const site = process?.env?.PUBLIC_BASE_URL || '/'
-console.log('site', site)
-console.log('process.env', process?.env)
 
 export default defineConfig({
   integrations: [astroI18next()],
